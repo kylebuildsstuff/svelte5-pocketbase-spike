75b41ee116361eabeb5ad974af13c5a2856372e8
Kyle Truong <ktruong008@gmail.com>
2024-06-02 16:10:58 -0400

    Add mode toggle and a couple shadcn components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,13 @@
 <script>
-	import '../app.css';
+  import { ModeWatcher } from 'mode-watcher';
+
+  import ModeToggle from '$lib/components/mode-toggle/mode-toggle.svelte';
+  import '../app.css';
 </script>
+
+<ModeWatcher />
+
+<ModeToggle />
 
 <slot></slot>
 
